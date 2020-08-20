14050b4a2f63e56b7e986c4dffbd9f8be53f05b4
DWarke <dwarke@hayesco.com>
2020-08-20 09:57:06 -0500

    fix: Remove whitespaces from email addresses

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,8 +24,8 @@ exports.signup = async (req, res, next) => {
 			throw error;
 		}
 		
-		const email = req.body.email.toLowerCase();
-		const username = req.body.username;
+		const email = req.body.email.toLowerCase().trim();
+		const username = req.body.username.trim();
 		const password = req.body.password;
 		
 		const hashedPw = await bcrypt.hash(password, 12);
@@ -51,7 +51,7 @@ exports.signup = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
-	const email = req.body.email.toLowerCase();
+	const email = req.body.email.toLowerCase().trim();
 	const password = req.body.password;
 	
 	try {
@@ -94,7 +94,7 @@ exports.login = async (req, res, next) => {
 };
 
 exports.forgotPassword = async (req, res, next) => {
-	const email = req.body.email.toLowerCase();
+	const email = req.body.email.toLowerCase().trim();
 	
 	try {
 		const user = await User.findOne({ email: email });
